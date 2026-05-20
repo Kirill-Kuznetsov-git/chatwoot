bf2911d5c40b57ecf690feea6abc85125667008e
Кирилл Кузнецов <kirill.cuznetsov2002@yandex.ru>
2026-05-20 19:12:51 +0300

    Raise widget conversations rate limit from 6 to 50 per 12h

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -175,7 +175,7 @@ class Rack::Attack
   # TODO: Deprecate this feature in future after finding a better solution
   if ActiveModel::Type::Boolean.new.cast(ENV.fetch('ENABLE_RACK_ATTACK_WIDGET_API', true))
     ## Prevent Conversation Bombing on Widget APIs ###
-    throttle('api/v1/widget/conversations', limit: 6, period: 12.hours) do |req|
+    throttle('api/v1/widget/conversations', limit: 50, period: 12.hours) do |req|
       req.ip if req.path_without_extentions == '/api/v1/widget/conversations' && req.post?
     end
 
